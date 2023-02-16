646bb9074e19959b0fdedc166bdb63c687bf18d4
Karim Aziz <karimaziz69@outlook.sa>
2023-02-16 02:29:22 +0200

    First-Commitment

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function active() {
+    var element = document.getElementById("main-menu");
+    element.classList.add("active");
+}
+// document.addEventListener("contextmenu", function(event) {
+//     event.preventDefault();
+//     alert("Right click is disabled on this website.");
+//   });
